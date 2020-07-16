f165e08a7afc20f5be076ff51828b7046dd40c7a
Jeswin Mathew <jeswinmathewkk@gmail.com>
2020-07-16 16:28:00 -0500

    Setup basic Application and Entry Point.

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+// For use by Hazel Applications
+
+#include "Hazel/Application.h"
+
+// ---Entry Point ----------------------------
+#include "Hazel/EntryPoint.h"
+// -------------------------------------------
+
+
